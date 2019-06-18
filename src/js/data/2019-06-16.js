7fe60c26b2c2fe361982874f4910905e2990fea3
Couatl <couatl_@outlook.com>
2019-06-17 17:26:03 -0700

    Added like, a lot of stuff.

--- src/js/data/2019-06-16.js
+++ src/js/data/2019-06-16.js
@@ -3,17 +3,20 @@ dataSet[dataSetVersion] = {};
 
 dataSet[dataSetVersion].options = [
   {
-    name: "Filter by Series Entry",
+    name: "Filter by Group",
     key: "series",
-    tooltip: "Check this to restrict to certain series.",
+    tooltip: "Check this to restrict to certain units, places of appearence, etc.",
     checked: false,
     sub: [
-      { name: "501st", key: "501" },
-      { name: "502nd", key: "502" },
-      { name: "503rd", key: "503" },
-      { name: "504th", key: "504" },
-      { name: "506th", key: "506" },
-	  { name: "31st Joint Fighter Squadron Afrika", key: "Afrika" },
+      { name: "501st JFW", key: "501" },
+      { name: "502nd JFW", key: "502" },
+      { name: "503rd JFW", key: "503" },
+      { name: "504th JFW", key: "504" },
+	  { name: "505th JFW", key: "505" },
+      { name: "506th JFW", key: "506" },
+	  { name: "507th JFW", key: "507" },
+	  { name: "Isle of Wight", tooltip: "Isle of Wight Detachment Group", key: "Wight" },
+	  { name: "OVA 2 [Africa]", tooltip: "Witches who appear in the second OVA", key: "Afrika" },
       { name: "Others", tooltip: "WIP (Please suggest further categories!)", key: "Others" },
     ]
   },
@@ -354,10 +357,72 @@ dataSet[dataSetVersion].characterData = [
 	  loli: true
     }
   },
-
+  
   //
   
-  // 506th because the 505th is kinda empty tbh
+  // 505th
+  
+  //
+  
+  {
+    name: "Grete M. Gollob",
+    img: "V02ddo1.png",
+    opts: {
+      series: ["505"],
+	  nanime: true
+    }
+  },
+  {
+    name: "Alya V. Alelyuhin",
+    img: "4OG6hbx.png",
+    opts: {
+      series: ["505"],
+	  nanime: true
+    }
+  },
+  {
+    name: "Constantia Cantacuzino",
+    img: "u8rfVhX.png",
+    opts: {
+      series: ["505"],
+	  nanime: true
+    }
+  },
+  {
+    name: "Stoyana Stoyanov",
+    img: "4OG6hbx.png",
+    opts: {
+      series: ["505"],
+	  nanime: true
+    }
+  },
+  {
+    name: "Vasilissa M. Vassiliades",
+    img: "4OG6hbx.png",
+    opts: {
+      series: ["505"],
+	  nanime: true
+    }
+  },
+  {
+    name: "Inufusa Yuno",
+    img: "mOdGNZh.png",
+    opts: {
+      series: ["505"],
+	  nanime: true
+    }
+  },
+  {
+    name: "Kuroe Ayaka",
+    img: "WOsEoxb.png",
+    opts: {
+      series: ["505"],
+	  nanime: true
+    }
+  },
+  //
+  
+  // 506th
   
   //
   {
@@ -430,6 +495,161 @@ dataSet[dataSetVersion].characterData = [
 	  nanime: true
     }
   },
+  
+  //
+  
+  // 507th, or my spirit fighter wing
+  
+  //
+  
+  {
+    name: "Anabuki Tomoko",
+    img: "VFbo7Uy.png",
+    opts: {	
+      series: ["507"],
+	  nanime: true
+    }
+  },
+  {
+    name: "Elizabeth F. Beurling",
+    img: "kLfRAXd.png",
+    opts: {	
+      series: ["507"],
+	  nanime: true
+    }
+  },
+  {
+    name: "Elma Leivonen",
+    img: "LCZz35B.png",
+    opts: {	
+      series: ["507"],
+	  nanime: true
+    }
+  },
+  {
+    name: "Giuseppina Cenni",
+    img: "lyaqpYL.png",
+    opts: {	
+      series: ["507"],
+	  nanime: true
+    }
+  },
+  {
+    name: "Katharine O'Hare", //i hate her
+    img: "KwLrIke.png",
+    opts: {	
+      series: ["507"],
+	  nanime: true
+    }
+  },
+  {
+    name: "Ursula Hartmann",
+    img: "eB2iasP.png",
+    opts: {	
+      series: ["507"],
+	  loli: true
+    }
+  },
+  {
+    name: "Sakomizu Haruka",
+    img: "HR2T4j9.png",
+    opts: {	
+      series: ["507"],
+	  loli: true,
+	  nanime: true
+    }
+  },
+  
+  //
+  
+  // Isle of Wight - bestos
+  
+  //
+  
+  {
+    name: "Kadomaru Misa",
+    img: "LZGH38Q.png",
+    opts: {	
+      series: ["Wight"],
+	  nanime: true
+    }
+  },
+  {
+    name: "Wilma Bishop",
+    img: "u6Z75A4.png",
+    opts: {	
+      series: ["Wight"],
+	  nanime: true
+    }
+  },
+  {
+    name: "Amelie Planchard",
+    img: "SuzV6ae.png",
+    opts: {	
+      series: ["Wight"],
+	  nanime: true
+    }
+  },
+  {
+    name: "Francie Gerard",
+    img: "UFecOx9.png",
+    opts: {	
+      series: ["Wight"],
+	  nanime: true
+    }
+  },
+  {
+    name: "Laura Toth",
+    img: "qvGkIeh.png",
+    opts: {	
+      series: ["Wight"],
+	  nanime: true
+    }
+  },
+  
+  
+  //
+  
+  // OVA 2 African Witches (because the amount of witches in africa is actually kinda confusing and there's a lot of non-canon stuff i don't want to deal with)
+  
+  //
+  
+  {
+    name: "Hanna Justina Marseille",
+    img: "HRzD6gP.png",
+    opts: {	
+      series: ["Afrika"]
+    }
+  },
+  {
+    name: "Raisa Pöttgen",
+    img: "sXRlWd7.png",
+    opts: {	
+      series: ["Afrika"]
+    }
+  },
+  {
+    name: "Edytha Neumann",
+    img: "qoewz44.png",
+    opts: {	
+      series: ["Afrika"]
+    }
+  },
+  
+  //
+  
+  // Others
+  
+  //
+  
+  {
+    name: "Heidemarie W. Schnaufer",
+    img: "3IP2y2n.png",
+    opts: {	
+      series: ["Others"]
+    }
+  },
+  
   
   
   
